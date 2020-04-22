8d8f162bada28ef163931c016181a4a88ab0958c
Aurelien Michaud <aurelien.michaud98@gmail.com>
2020-04-22 15:15:48 +0200

    updated alu

--- ALU.vhd
+++ ALU.vhd
@@ -7,12 +7,12 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity ALU is
 	Port(
-		OPERAND1	: in 	STD_LOGIC_VECTOR (7 downto 0);	--< The first operand in the arithmetic operation
-		OPERAND2	: in 	STD_LOGIC_VECTOR (7 downto 0);	--< The second operand in the arithmetic operation
+		OPERAND1	: in 	STD_LOGIC_VECTOR(7 downto 0);	--< The first operand in the arithmetic operation
+		OPERAND2	: in 	STD_LOGIC_VECTOR(7 downto 0);	--< The second operand in the arithmetic operation
 
-		ALU_CTRL	: in 	STD_LOGIC_VECTOR (2 downto 0);	--< The arithmetic operation to perform
+		ALU_CTRL	: in 	STD_LOGIC_VECTOR(2 downto 0);	--< The arithmetic operation to perform
 
-		RESULT	: out STD_LOGIC_VECTOR (7 downto 0);	--> The final result of the arithmetic operation
+		RESULT	: out STD_LOGIC_VECTOR(7 downto 0);	--> The final result of the arithmetic operation
 
 		NEG_FLAG	: out STD_LOGIC;			--> Negative FLAG : 1 when the result is negative, 0 otherwise
 		OVF_FLAG	: out STD_LOGIC;			--> Overflow FLAG : 1 when the result overflows the RESULT capacity, 0 otherwise
@@ -23,18 +23,25 @@ end ALU;
 
 architecture Behavioral of ALU is 
 
-	signal RESULT_TMP : STD_LOGIC_VECTOR (15 downto 0);
+	signal RESULT_TMP : STD_LOGIC_VECTOR(15 downto 0);
 
 	-- ALU_CTRL's possible values
-	constant ALU_ADD : STD_LOGIC_VECTOR(2 downto 0) := "000";
-	constant ALU_SUB : STD_LOGIC_VECTOR(2 downto 0) := "001";
-	constant ALU_MUL : STD_LOGIC_VECTOR(2 downto 0) := "010";
+	constant ALU_ADD 	: STD_LOGIC_VECTOR(2 downto 0) := "000";
+	constant ALU_SUB 	: STD_LOGIC_VECTOR(2 downto 0) := "001";
+	constant ALU_MUL 	: STD_LOGIC_VECTOR(2 downto 0) := "010";
+	
+	constant ALU_AND 	: STD_LOGIC_VECTOR(2 downto 0) := "011";
+	constant ALU_XOR 	: STD_LOGIC_VECTOR(2 downto 0) := "100";
+	constant ALU_OR	: STD_LOGIC_VECTOR(2 downto 0) := "101";
 
 begin 
 
 	RESULT_TMP <=	(X"00" & OPERAND1) + (X"00" & OPERAND2) when ALU_CTRL = ALU_ADD else
 						(X"00" & OPERAND1) - (X"00" & OPERAND2) when ALU_CTRL = ALU_SUB else
 						std_logic_vector(to_unsigned(to_integer(unsigned(OPERAND1)) * to_integer(unsigned(OPERAND2)), 16)) when ALU_CTRL = ALU_MUL else
+						(X"00" & OPERAND1) and (X"00" & OPERAND2) when ALU_CTRL = ALU_AND else
+						(X"00" & OPERAND1) xor (X"00" & OPERAND2) when ALU_CTRL = ALU_XOR else
+						(X"00" & OPERAND1) or (X"00" & OPERAND2) 	when ALU_CTRL = ALU_OR else
 						(others => '0');
 
 	RESULT <= RESULT_TMP(7 downto 0);
@@ -44,9 +51,9 @@ begin
 	OVF_FLAG <= '1' when ALU_CTRL = ALU_ADD and (OPERAND1(7) = OPERAND2(7) and RESULT_TMP(7) /= OPERAND1(7)) else
 					'1' when ALU_CTRL = ALU_SUB and (OPERAND1(7) /= OPERAND2(7) and RESULT_TMP(7) /= OPERAND1(7)) else
 					'1' when ALU_CTRL = ALU_MUL and (
-									(OPERAND1(7) = OPERAND2(7) and RESULT_TMP(7) = '1')
-									or
-									(OPERAND1(7) /= OPERAND2(7) and RESULT_TMP(7) = '0')
+																(OPERAND1(7) = OPERAND2(7) and RESULT_TMP(7) = '1')
+																or
+																(OPERAND1(7) /= OPERAND2(7) and RESULT_TMP(7) = '0')
 																) else
 					'0';
 

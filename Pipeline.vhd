736064eddfed8c5fba23864ee217058a29b2ce79
Aurelien Michaud <aurelien.michaud98@gmail.com>
2020-04-30 14:26:59 +0200

    merged

--- Pipeline.vhd
+++ Pipeline.vhd
@@ -1,6 +1,5 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-<<<<<<< HEAD
 use IEEE.NUMERIC_STD.ALL;
 
 library work;
@@ -55,32 +54,3 @@ begin
 
 end Behavioral;
 
-=======
-
-//A chaque tic d'horloge, le rôle du pipeline est de propager les entrées
-entity Pipeline is
-
-    Port 
-			  OPin : in  std_logic_vector(7 downto 0);
-           Ain : in  std_logic_vector(7 downto 0);
-           Bin : in  std_logic_vector(7 downto 0);
-           Cin : in  std_logic_vector(7 downto 0);
-           OPout : out  std_logic_vector(7 downto 0);
-           Aout : out  std_logic_vector(7 downto 0);
-           Bout : out  std_logic_vector(7 downto 0);
-           Cout : out  std_logic_vector(7 downto 0)
-	);
-end Pipeline;
-
-architecture Behavioral of Pipeline is
-begin
-	process (clk)
-	begin
-		if clk'event and clk = '1' then
-			OPout <= OPin;
-			Aout <= Ain;
-			Bout <= Bin;
-			Cout <= Cin;
-	end process;
-end Behavioral;
->>>>>>> 1ce0e57cea54c23407654ddef37a50a3b15c9c3d
